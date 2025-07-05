f6064f8af91de01686410f7ffa4c0bc0410b0c22
Arbaaz Kagdi <arbaazkagdi@gmail.com>
2025-07-05 18:28:55 +0530

    tv-show-advisor code update

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import s from "./style.module.css"
+export function App() {
+    return <div className={s.main_container}>
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>LOGO</div>
+            <div>Subtitle</div>
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{ width: "100%" }} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_detail}>Tv show detail</div>
+      <div className={s.recommended_tv_shows}>Recommended tv shows</div>
+    </div>
+  }
