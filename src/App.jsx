df0ef9e993ed59325a44cc2445012a05c05801e3
Arbaaz Kagdi <arbaazkagdi@gmail.com>
2025-07-11 15:00:59 +0530

    logo component implimentation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { TVShowAPI } from "./api/tv-show";
 import s from "./style.module.css";
 import { BACKDROP_BASE_URL } from "./config";
 import { TvShowDetail } from "./components/TvShowDetail/TvShowDetail.jsx";
+import { Logo } from "./components/Logo/Logo.jsx";
+import logoImg from "./assets/images/icons8-tv-60.png";
 
 export function App() {
   const [currentTVShow, setCurrentTVShow] = useState();
@@ -31,8 +33,11 @@ export function App() {
       <div className={s.header}>
         <div className="row">
           <div className="col-4">
-            <div>LOGO</div>
-            <div>Subtitle</div>
+            <Logo
+              img={logoImg}
+              title={"Watowatch"}
+              subtitle={"Find a show you may like"}
+            ></Logo>
           </div>
           <div className="col-md-12 col-lg-4">
             <input style={{ width: "100%" }} type="text" />
