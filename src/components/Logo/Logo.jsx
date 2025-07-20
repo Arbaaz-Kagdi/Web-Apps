f16b70a010340f304c4b70252d905b6445d33b95
Arbaaz Kagdi <arbaazkagdi@gmail.com>
2025-07-20 16:01:26 +0530

    return to home on click of logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,18 @@
 import s from "./style.module.css";
 
 export function Logo({ title, subtitle, img }) {
+  function toggleHome() {
+    window.location.reload();
+  }
   return (
     <>
-      <div className={s.flexContainer}>
-        <img  src={img} alt="Logo" className={s.img} />
-        <div className={s.title}>{title}</div>
+      <div>
+        <div className={s.flexContainer} onClick={toggleHome}>
+          <img src={img} alt="Logo" className={s.img} />
+          <div className={s.title}>{title}</div>
+        </div>
+        <div className={s.subtitle}>{subtitle}</div>
       </div>
-      <div className={s.subtitle}>{subtitle}</div>
     </>
   );
 }
