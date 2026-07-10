86dd0a7d65710f0e2e73e14b3b37bb0d38fb5b66
Arbaaz Kagdi <arbaazkagdi@gmail.com>
2026-07-10 23:00:01 +0530

    comments update

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -56,7 +56,7 @@ export function Contact() {
       .then(() => {
         setStatus('success');
         setFormData(INITIAL_FORM);
-        showToast('Success', 'Message sent! You will get a response withing 3 working days.');
+        showToast('Success', 'Message sent! You will get a response within 3 working days.');
       })
       .catch((err) => {
         console.error('EmailJS error:', err);
